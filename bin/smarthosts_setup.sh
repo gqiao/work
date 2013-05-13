b341f85eb26b399da6c03022e46faa2a90e8d607
George Qiao <joesensport@gmail.com>
2013-05-13 10:58:21 +0800

    add smarthosts_setup.sh

---
+++ bin/smarthosts_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ ! -e /etc/hosts.bak ]; then
+    sudo cp /etc/hosts /etc/hosts.bak
+fi
+
+sudo wget https://smarthosts.googlecode.com/svn/trunk/hosts
+sudo mv ./hosts /etc/hosts
+cat /etc/hosts
+
