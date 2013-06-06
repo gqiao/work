4fce9924ae09719902e9b27f0800ac73f35292b1
George Qiao <joesensport@gmail.com>
2013-06-06 15:57:50 +0800

    add ruby on rails demo, depot\!

---
+++ src/rails/depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'c0bef3820da83ef66f5f247a2d415cde022671e322f73ab7742b802e15a402224699156182e044500a0f8baf8bac1e6381fb0f5ab8560145895d992cbd177e3a'
