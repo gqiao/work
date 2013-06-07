3bace3248e4b7a45abd61c4f86fefa17aa16c91e
George Qiao <joesensport@gmail.com>
2013-06-08 07:50:55 +0800

    TaskD play time: activitys

--- src/rails/depot/app/controllers/line_items_controller.rb
+++ src/rails/depot/app/controllers/line_items_controller.rb
@@ -17,35 +17,35 @@
 class LineItemsController < ApplicationController
   #attr_accessible :created_at, :updated_at
   # GET /line_items
-  # GET /line_items.xml
+  # GET /line_items.json
   def index
     @line_items = LineItem.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @line_items }
+      format.json  { render :json => @line_items }
     end
   end
 
   # GET /line_items/1
-  # GET /line_items/1.xml
+  # GET /line_items/1.json
   def show
     @line_item = LineItem.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @line_item }
+      format.json  { render :json => @line_item }
     end
   end
 
   # GET /line_items/new
-  # GET /line_items/new.xml
+  # GET /line_items/new.json
   def new
     @line_item = LineItem.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @line_item }
+      format.json  { render :json => @line_item }
     end
   end
 
@@ -55,7 +55,7 @@ class LineItemsController < ApplicationController
   end
 
   # POST /line_items
-  # POST /line_items.xml
+  # POST /line_items.json
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
@@ -66,41 +66,41 @@ class LineItemsController < ApplicationController
       if @line_item.save
         format.html { redirect_to(@line_item.cart,
           :notice => 'Line item was successfully created.') }
-        format.xml  { render :xml => @line_item,
+        format.json  { render :json => @line_item,
           :status => :created, :location => @line_item }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @line_item.errors,
+        format.json  { render :json => @line_item.errors,
           :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /line_items/1
-  # PUT /line_items/1.xml
+  # PUT /line_items/1.json
   def update
     @line_item = LineItem.find(params[:id])
 
     respond_to do |format|
       if @line_item.update_attributes(params[:line_item])
         format.html { redirect_to(@line_item, :notice => 'Line item was successfully updated.') }
-        format.xml  { head :ok }
+        format.json  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @line_item.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @line_item.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /line_items/1
-  # DELETE /line_items/1.xml
+  # DELETE /line_items/1.json
   def destroy
     @line_item = LineItem.find(params[:id])
     @line_item.destroy
 
     respond_to do |format|
       format.html { redirect_to(line_items_url) }
-      format.xml  { head :ok }
+      format.json  { head :ok }
     end
   end
 end
--- src/rails/depot/app/controllers/store_controller.rb
+++ src/rails/depot/app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @count = increment_count
+  end
+
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
   end
 end
