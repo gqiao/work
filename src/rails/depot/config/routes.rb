43599722c0f005987cfc0cde726860135f61a628
George Qiao <joesensport@gmail.com>
2013-06-07 10:58:25 +0800

    add root site: app/views/store/index.html

--- src/rails/depot/config/routes.rb
+++ src/rails/depot/config/routes.rb
@@ -1,4 +1,6 @@
 Depot::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
 
@@ -52,10 +54,12 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'store#index', :as => 'store'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
 end
