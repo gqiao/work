38d53e398696c8b54ff2f7840b5bdcf65cf74135
George Qiao <joesensport@gmail.com>
2013-09-04 08:41:56 +0800

    add rename scripts for rename all file folder

---
+++ bin/rename.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+
+usage() {
+    echo "$0 -t|--try -h|--help"
+    echo "Example:"
+    echo "$0 --try 01 02 03 04 05 06 07 08 09 10 11 12"
+    echo "$0       01 02 03 04 05 06 07 08 09 10 11 12"
+}
+
+if [ -z ${2} ]; then
+    usage
+    exit 1
+fi
+
+TRY="no"
+
+args=`getopt -o ht: -l help,try: -- "$@"`
+
+while true; do
+    case "${1}" in
+    -t|--try)
+        TRY="yes"
+        shift ;;
+    -h|--help)
+        usage
+        exit 0 ;;
+    *) break ;;
+    esac
+done
+
+
+
+firstnewname=$1
+shift
+lastnewname=$1
+shift
+
+echo "newname = ${firstnewname}XXX${lastnewname}"
+echo "\$@ = ${@}"
+# $@ = 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+
+
+for i in $@; do
+    filename=`find . -name "*${i}.mp3"`
+    #ls -l "${filename}"
+    if [ "${TRY}" = "no" ]; then
+        echo "try is no , so do it!"
+        mv "${filename}" "${firstnewname}${i}${lastnewname}"
+    else
+        echo "oldname = ${filename} newname = ${firstnewname}${i}${lastnewname}"
+    fi
+done
